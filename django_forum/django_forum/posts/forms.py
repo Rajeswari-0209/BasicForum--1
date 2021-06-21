016ff7d8becb56df8d28aa4487c7958d0a672867
Oshae Alexis <terongp3@gmail.com>
2021-06-20 22:37:31 -0400

    first commit

---
+++ django_forum/django_forum/posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import fields
+from .models import Post
+from django.forms import ModelForm
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
